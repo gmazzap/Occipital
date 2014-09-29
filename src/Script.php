c0856c2be0fcdd6e8dfbc0c3c82fe635c36cb3cb
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-29 11:20:30 +0200

    Fixed Script setLocalizeData signature

--- src/Script.php
+++ src/Script.php
@@ -9,20 +9,20 @@ class Script extends Enqueuable implements ScriptInterface {
         return $this->data;
     }
 
-    public function isFooter( $set = NULL ) {
-        if ( ! is_null( $set ) ) {
-            $this->is_footer = ! empty( $set );
-        }
-        return is_null( $set ) ? $this->is_footer : $this;
-    }
-
-    public function setLocalizeData( stdClass $data ) {
+    public function setLocalizeData( $data ) {
         if ( isset( $data->name ) && isset( $data->data ) && is_string( $data->name ) ) {
             $data->name = filter_var( $data->data, FILTER_SANITIZE_STRING );
             $data->data = (array) $data->data;
             $this->data = $data;
         }
         return $this;
+    }
+
+    public function isFooter( $set = NULL ) {
+        if ( ! is_null( $set ) ) {
+            $this->is_footer = ! empty( $set );
+        }
+        return is_null( $set ) ? $this->is_footer : $this;
     }
 
 }
