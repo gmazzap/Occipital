79f02114f425aad8b9f5a0ef8d63cff932fad105
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-29 11:18:45 +0200

    Fixed EnqueuerInterface method signature

--- src/EnqueuerInterface.php
+++ src/EnqueuerInterface.php
@@ -5,16 +5,16 @@ interface EnqueuerInterface {
     /**
      * Enqueue the given script objects, extracting properties and calling `wp_enqueue_script`.
      *
-     * @param \SplObjectStorage $scripts
+     * @param \SplObjectStorage $scripts_factory
      */
-    public function enqueueScripts( \SplObjectStorage $scripts );
+    public function enqueueScripts( \Closure $scripts_factory );
 
     /**
      * Enqueue the given script objects, extracting properties and calling `wp_enqueue_style`.
      *
-     * @param \SplObjectStorage $styles
+     * @param \SplObjectStorage $styles_factory
      */
-    public function enqueueStyles( \SplObjectStorage $styles );
+    public function enqueueStyles( \Closure $styles_factory );
 
     /**
      * Set asdone the styles and the scripts provided by registered scripts and styles.
