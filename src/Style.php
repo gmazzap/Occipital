dcabed64b93c4771c19313d6e85ea098f6b32711
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-09-30 13:10:05 +0200

    Cosmetic

--- src/Style.php
+++ src/Style.php
@@ -9,18 +9,8 @@ class Style extends Enqueuable implements StyleInterface {
     }
 
     public function setMedia( $media ) {
-        $medias = [
-            'all',
-            'aural',
-            'braille',
-            'handheld',
-            'projection',
-            'print',
-            'screen',
-            'tty',
-            'tv'
-        ];
-        if ( is_string( $media ) && in_array( strtolower( $media ), $medias, TRUE ) ) {
+        $ok = [ 'all', 'aural', 'braille', 'handheld', 'projection', 'print', 'screen', 'tty', 'tv' ];
+        if ( is_string( $media ) && in_array( strtolower( $media ), $ok, TRUE ) ) {
             $this->media = strtolower( $media );
         }
         return $this;
