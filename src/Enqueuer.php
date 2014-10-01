243219a0ff0ffa7e06b8898935ac24d31626303f
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-10-01 16:55:01 +0200

    Refactored provided registering for nested deps issue

--- src/Enqueuer.php
+++ src/Enqueuer.php
@@ -2,7 +2,7 @@
 
 class Enqueuer implements EnqueuerInterface {
 
-    private $provided = [ 'script' => [ ], 'styles' => [ ] ];
+    private $provided = [ 'scripts' => [ ], 'styles' => [ ] ];
 
     public function enqueueScripts( \Closure $scripts_factory ) {
         /** @var \Brain\Occipital\FilterInterface $scripts */
@@ -47,15 +47,17 @@ class Enqueuer implements EnqueuerInterface {
             return;
         }
         global $wp_scripts, $wp_styles;
-        $done_scripts = $this->getProvided( 'scripts' );
         $done_styles = $this->getProvided( 'styles' );
-        if ( $wp_scripts instanceof \WP_Scripts ) {
-            $wp_scripts->to_do = array_values( array_diff( $wp_scripts->to_do, $done_scripts ) );
-            $wp_scripts->done = $done_scripts;
-        }
-        if ( $wp_styles instanceof \WP_Styles ) {
+        if ( $wp_styles instanceof \WP_Styles && ! empty( $done_styles ) ) {
+            $this->ensureStylesDeps( $done_styles );
             $wp_styles->to_do = array_values( array_diff( $wp_styles->to_do, $done_styles ) );
             $wp_styles->done = $done_styles;
+        }
+        $done_scripts = $this->getProvided( 'scripts' );
+        if ( $wp_scripts instanceof \WP_Scripts && ! empty( $done_scripts ) ) {
+            $this->ensureScriptsDeps( $done_scripts );
+            $wp_scripts->to_do = array_values( array_diff( $wp_scripts->to_do, $done_scripts ) );
+            $wp_scripts->done = $done_scripts;
         }
         return TRUE;
     }
@@ -75,6 +77,36 @@ class Enqueuer implements EnqueuerInterface {
         $args = [ $asset->getHandle(), $asset->getSrc(), $asset->getDeps(), $asset->getVer() ];
         $args[] = $asset instanceof ScriptInterface ? $asset->isFooter() : $asset->getMedia();
         return $args;
+    }
+
+    private function ensureStylesDeps( Array $provided ) {
+        $deps = [ ];
+        array_walk( $provided, function( $id ) use(&$deps) {
+            if ( wp_style_is( $id, 'registered' ) ) {
+                $deps = array_merge( $deps, $GLOBALS[ 'wp_styles' ]->registered[ $id ]->deps );
+            }
+        } );
+        $enqueue = array_filter( array_unique( $deps ), function($id) {
+            return wp_style_is( $id, 'registered' ) && ! wp_style_is( $id, 'queue' );
+        } );
+        array_walk( $enqueue, function($dep) {
+            wp_enqueue_style( $dep );
+        } );
+    }
+
+    private function ensureScriptsDeps( Array $provided ) {
+        $deps = [ ];
+        array_walk( $provided, function( $id ) use(&$deps) {
+            if ( wp_script_is( $id, 'registered' ) ) {
+                $deps = array_merge( $deps, $GLOBALS[ 'wp_script' ]->registered[ $id ]->deps );
+            }
+        } );
+        $enqueue = array_filter( array_unique( $deps ), function($id) {
+            return wp_script_is( $id, 'registered' ) && ! wp_script_is( $id, 'queue' );
+        } );
+        array_walk( $enqueue, function($dep) {
+            wp_enqueue_script( $dep );
+        } );
     }
 
 }
