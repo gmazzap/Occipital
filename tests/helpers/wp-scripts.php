32cbe272e869622cde92531b4ca0f7321ce41a3c
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-10-02 04:54:01 +0200

    added function sto test helpers classes

--- tests/helpers/wp-scripts.php
+++ tests/helpers/wp-scripts.php
@@ -6,5 +6,21 @@ class WP_Scripts {
     public $to_do = [ ];
     public $done = [ ];
     public $registered = [ ];
+    public $extra = [ ];
+
+    public function get_data( $handle, $key ) {
+        $extra = isset( $extra[ $handle ] ) ? $extra[ $handle ] : FALSE;
+        return $extra && isset( $extra[ $key ] ) ? $extra[ $key ] : FALSE;
+    }
+
+    public function add_data( $handle, $key, $value ) {
+        if ( ! isset( $this->extra[ $handle ] ) ) {
+            $this->extra[ $handle ] = [ ];
+        }
+        if ( ! isset( $this->extra[ $handle ][ $key ] ) ) {
+            $this->extra[ $handle ][ $key ] = [ ];
+        }
+        $this->extra[ $handle ][ $key ] = $value;
+    }
 
 }
