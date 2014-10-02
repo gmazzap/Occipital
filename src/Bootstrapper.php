0033deb48b79350cd6c1f15267b22e62facfd516
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-10-02 22:22:34 +0200

    Minor Bootstrapper edit

--- src/Bootstrapper.php
+++ src/Bootstrapper.php
@@ -49,28 +49,29 @@ class Bootstrapper implements BootstrapperInterface {
             $this->getContainer()->setSide( $this->getSide() );
             do_action( 'brain_assets_ready', $side, $this->getContainer() );
             do_action( "brain_assets_ready_{$side}", $this->getContainer() );
-            $sides = [ ContainerInterface::LOGIN, ContainerInterface::ADMIN, ContainerInterface::FRONT ];
             do_action( 'brain_assets_remove', $this->getContainer() );
             do_action( "brain_assets_remove_{$side}", $this->getContainer() );
-            $this->buildAssetsIterators();
+            $this->buildAssetsIterator();
             do_action( 'brain_assets_done' );
         }, '-' . PHP_INT_MAX );
     }
 
-    private function buildAssetsIterators() {
+    private function buildAssetsIterator() {
         $side = $this->getSide();
         $side_styles = $this->getContainer()->getStyles( $side );
         $all_styles = $this->getContainer()->getStyles( Container::ALL );
         $side_scripts = $this->getContainer()->getScripts( $side );
         $all_scripts = $this->getContainer()->getScripts( Container::ALL );
-        $this->getContainer()->setSideStyles( $this->mergeAssets( $side_styles, $all_styles ) );
-        $this->getContainer()->setSideScripts( $this->mergeAssets( $side_scripts, $all_scripts ) );
+        $iterator = $this->mergeAssets( $side_styles, $all_styles, $side_scripts, $all_scripts );
+        $this->getContainer()->setAssetsIterator( $iterator );
     }
 
-    private function mergeAssets( \Iterator $side, \Iterator $all ) {
+    private function mergeAssets( $side_styles, $all_styles, $side_scripts, $all_scripts ) {
         $iterator = new \AppendIterator();
-        $iterator->append( $side );
-        $iterator->append( $all );
+        $iterator->append( $side_styles );
+        $iterator->append( $all_styles );
+        $iterator->append( $side_scripts );
+        $iterator->append( $all_scripts );
         return $iterator;
     }
 
